eb870144942105ee18de0753b49969141181110c
Jorge Costa <jorgeefrazaocosta@gmail.com>
2022-12-01 22:39:35 +0000

    Improve left inset

--- Sources/DropView.swift
+++ Sources/DropView.swift
@@ -67,7 +67,11 @@ internal final class DropView: UIView {
         
         var insets = UIEdgeInsets(top: 7.5, left: 12.5, bottom: 7.5, right: 12.5)
         
-        insets.left = 40
+        if drop.icon == nil {
+            insets.left = 40
+        } else {
+            insets.left = 20
+        }
         
         if drop.action?.icon == nil {
             insets.right = 40
